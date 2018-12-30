72ae19ab489f9067b82ba83cced2671cd8d95206
Deepak Kumar Singh <deepakexodia@gmail.com>
2018-12-30 18:17:28 +0530

    added post code form

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,6 +134,63 @@ class Form extends Component {
     )
   }
 }
+
+class PostOfficeForm extends Component {
+  state = {
+    input: { value: null },
+    search: {
+      isDisabled: true,
+      isLoading: false,
+    },
+  }
+
+  handleChange = event => {
+    var val = event.target.value
+    val = val.match(/d+/g)
+    // val = (isNaN(val) | (val === 0)) & ''
+
+    if (val.length === 6)
+      this.setState(prevState => {
+        return {
+          input: { value: val },
+          search: { ...prevState.search, isDisabled: false },
+        }
+      })
+    else
+      this.setState(prevState => {
+        return {
+          input: { value: val },
+          search: { ...prevState.search, isDisabled: true },
+        }
+      })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  render = () => {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Pincode:</label>
+        <input
+          className="pincode"
+          type="text"
+          maxLength="6"
+          value={this.state.input.value}
+          onChange={this.handleChange}
+        />
+        <input
+          className={`btn-search ${
+            this.state.search.isDisabled ? 'is-disabled' : ''
+          }`}
+          type="submit"
+          value="Search"
+        />
+      </form>
+    )
+  }
+}
 export default () => (
   <Fragment>
     <NavBar />
@@ -148,25 +205,21 @@ export default () => (
           <Form />
         </TabPanel>
         <TabPanel>
-          <form>
-            <label>State:</label>
-            <Select className="select" options={options} />
-            <label>City:</label>
-            <Select className="select" options={options} />
-            <label>Locality:</label>
-            <Select className="select" options={options} />
-            <input className="btn-search" type="button" value="Search" />
-          </form>
+          <PostOfficeForm />
         </TabPanel>
       </Tabs>
       <Table>
         <Thead>
           <Tr>
-            {['Location', 'Pin Code', 'Pin Code', 'Pin Code', 'Pin Code'].map(
-              header => (
-                <Th key={header}>{header}</Th>
-              )
-            )}
+            {[
+              'Post Office Name',
+              'Pincode',
+              'Pin Code',
+              'Pin Code',
+              'Pin Code',
+            ].map(header => (
+              <Th key={header}>{header}</Th>
+            ))}
           </Tr>
         </Thead>
         <Tbody>
