85066a596606e77b74b75d5f82cfcc052608c9c4
Deepak Kumar Singh <deepakexodia@gmail.com>
2019-01-05 17:18:22 +0530

    corrected table display

--- src/elements/table.js
+++ src/elements/table.js
@@ -9,10 +9,11 @@ import {
   Td,
 } from 'react-super-responsive-table'
 
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 import './table.css'
 
 export default function Table({
-  headers = ['Post Office Name', 'Pincode', 'Pin Code', 'Pin Code', 'Pin Code'],
+  headers = ['Post Office', 'Pincode', 'Pin Code', 'Pin Code', 'Pin Code'],
   data = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]],
 }) {
   return (
