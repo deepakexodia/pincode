275770540a3fe7b9f6ba14c83f3aecde7dc85c0f
deepakkumarsingh <deepak17.singh@paytm.com>
2019-01-14 23:40:25 +0530

    aded website name, url and company name; commented local developer proxy server

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,24 +3,24 @@ var proxy = require('http-proxy-middleware')
 module.exports = {
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
-    app.use(
-      '/.netlify/functions/',
-      proxy({
-        target: 'http://localhost:9000',
-        pathRewrite: {
-          '/.netlify/functions/': '',
-        },
-      })
-    )
-  },
+  // developMiddleware: app => {
+  //   app.use(
+  //     '/.netlify/functions/',
+  //     proxy({
+  //       target: 'http://localhost:9000',
+  //       pathRewrite: {
+  //         '/.netlify/functions/': '',
+  //       },
+  //     })
+  //   )
+  // },
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    websiteName: 'Website Name',
-    companyName: 'Company Name',
-    websiteURL: 'website URL',
+    author: `Deepak Singh`,
+    websiteName: 'My Pincode',
+    companyName: 'My Pincode',
+    websiteURL: 'mypincode.netlify.com',
     email: 'deepakexodia@gmail.com',
   },
   plugins: [
