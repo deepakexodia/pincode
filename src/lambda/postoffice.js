1643967de9321d32bd3610775863cd649160298c
deepakkumarsingh <deepak17.singh@paytm.com>
2019-01-16 00:22:30 +0530

    added error handling

--- src/lambda/postoffice.js
+++ src/lambda/postoffice.js
@@ -4,13 +4,33 @@ export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters)
   const { pincode } = event.queryStringParameters
   fetch(`https://pincode.saratchandra.in/api/pincode/${pincode}`)
-    .then(resp => resp.json())
+    .then(resp => {
+      const { status } = resp.status
+      if (status === 200 || status === 404) {
+        return resp.json()
+      } else {
+        throw 'server issue'
+      }
+    })
     .then(json => {
-      console.log(json)
+      if (json.status === 200) {
+        callback(null, {
+          // return null to show no errors
+          statusCode: json.status,
+          body: JSON.stringify(json.data),
+        })
+      }
+      if (json.status === 404) {
+        callback(null, {
+          statusCode: json.status,
+          body: JSON.stringify(json.message),
+        })
+      }
+    })
+    .catch(err => {
       callback(null, {
-        // return null to show no errors
-        statusCode: 200, // http status code
-        body: JSON.stringify(json.data),
+        statusCode: 500,
+        body: JSON.stringify({ message: err}),
       })
     })
 }
